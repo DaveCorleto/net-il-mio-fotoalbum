1df75eba744e4f62d686efffe437def2c26b67f0
DaveCorleto <davide.corleto@fastwebnet.it>
2024-05-27 11:10:10 +0200

    Aggiungere i file di progetto.

---
+++ Models/Category.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace net_il_mio_fotoalbum.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List<Photo> Photos { get; set; }
+
+        public Category()
+        {
+            Photos = new List<Photo>(); // Inizializza Photos a una lista vuota
+        }
+
+        public Category(int id, string title, List<Photo> photos)
+        {
+            Id = id;
+            Title = title;
+            //Se photos è null, usa una nuova lista vuota di Photo; altrimenti, usa photos".
+            Photos = photos ?? new List<Photo>(); 
+        }
+    }
+}
+
+
+
---
+++ Models/Photo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace net_il_mio_fotoalbum.Models
+{
+
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public byte[] Image { get; set; } // Per memorizzare i dati binari dell'immagine
+        public bool IsVisible { get; set; }
+
+        // Relazione molti a molti con Category
+        public List<Category> Categories { get; set; }
+
+        // Costruttore senza parametri
+        public Photo()
+        {
+            Categories = new List<Category>();
+            IsVisible = true; // Imposta IsVisible a true per impostazione predefinita
+        }
+
+        // Costruttore con parametri, con valore predefinito per isVisible
+        public Photo(int id, string title, string description, byte[] image, List<Category> categories, bool isVisible = true)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Image = image;
+            IsVisible = isVisible; // Assegna il valore passato o usa il valore predefinito true
+            Categories = categories ?? new List<Category>(); // Gestisci il caso in cui categories sia null
+        }
+    }
+
+}
